27478b795520683beed148eb6271860a2a38bc75
Gill <leapin_leprechaun@hotmail.com>
2016-06-05 12:53:16 +0100

    updated get and post calls to accept form data and persist it

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,11 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Quote = require('../models/quote'); //bring in the quote model
 
-router.route('/') //this already runs off /user from where it was mounted
+var bodyParser = require('body-parser'); //needed to get form data in correctly
+var parseUrlencoded = bodyParser.urlencoded({extended:false}); //return value is a middleware function
+
+router.route('/') //this already runs off /quote from where it was mounted
 .get(function (request,response) {
- // response.json(Object.keys(blocks));
-  response.send('hello quotes');
+  mongoose.model('quotes').find(function(err, quotes){
+    if(err){
+      response.send(500, 'There was an error - tough luck.');
+    } else {
+      response.json(quotes);
+    }
+  })
 })
+.post(parseUrlencoded,function (request,response) {
+  var quote = new Quote({
+    content: request.body.content,
+    author: request.body.author
+  });
+
+  quote.save(function(err, model) {
+    if (err) {
+      response.send(500, 'There was an error - tough luck.');
+    } else {
+      response.redirect('/');
+    }
+  });
+});
 
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var User = require('../models/user'); //bring in the user model
+
+var bodyParser = require('body-parser'); //needed to get form data in correctly
+var parseUrlencoded = bodyParser.urlencoded({extended:false}); //return value is a middleware function
 
 router.route('/') //this already runs off /user from where it was mounted
 .get(function (request,response) {
- // response.json(Object.keys(blocks));
-  response.send('hello user');
+  mongoose.model('users').find(function(err, users){
+    if(err){
+      response.send(500, 'There was an error - tough luck.');
+    } else {
+      response.json(users);
+    }
+  });
+
 })
+.post(parseUrlencoded,function(request,response){
+  var user = new User({
+    name: request.body.name,
+    age: request.body.age,
+    location: request.body.location,
+    areasOfInterest: request.body.areasOfInterest
+  });
+
+  user.save(function(err, model) {
+    if (err) {
+      response.send(500, 'There was an error - tough luck.');
+    } else {
+      response.redirect('/');
+    }
+  });
+
+});
 
 
 module.exports = router;
